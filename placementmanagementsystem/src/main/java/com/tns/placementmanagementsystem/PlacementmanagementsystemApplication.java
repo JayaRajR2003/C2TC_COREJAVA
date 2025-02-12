a01765aece7b7f9dd6f7ca10bc3cb2690993a1be
JAYARAJ <JAYARAJ@KUROKO>
2025-02-12 08:34:35 +0530

    SpringBootProject1

---
+++ placementmanagementsystem/src/main/java/com/tns/placementmanagementsystem/PlacementmanagementsystemApplication.java
@@ -0,0 +1,15 @@
+
+package com.tns.placementmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementmanagementsystemApplication.class, args);
+	}
+
+
+}
---
+++ placementmanagementsystem/src/main/java/com/tns/placementmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.tns.placementmanagementsystem.repository;
+
+import com.tns.placementmanagementsystem.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByEmail(String email);
+}
+
+
